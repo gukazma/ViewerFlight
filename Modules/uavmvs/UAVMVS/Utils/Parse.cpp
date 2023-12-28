57aff6c4ac7f77bfa1673f666045e16665fec21a
gukazma <18017158729@163.com>
2023-12-28 21:52:03 +0800

    ✨ feat: 修复深圳大学未能读取的数据

--- Modules/uavmvs/UAVMVS/Utils/Parse.cpp
+++ Modules/uavmvs/UAVMVS/Utils/Parse.cpp
@@ -5,6 +5,9 @@
 #include <boost/property_tree/xml_parser.hpp>
 #include <osgDB/ReadFile>
 #include <osgEarth/GeoTransform>
+#include <QRegExp>
+#include <QString>
+#include <QStringList>
 namespace fs = boost::filesystem;
 osg::ref_ptr<osg::Node> ParseOSGBFile(const boost::filesystem::path& path_)
 {
@@ -45,20 +48,40 @@ osgEarth::GeoPoint ParseMetaDataFile(const boost::filesystem::path& path_)
     boost::property_tree::read_xml(path_.generic_string(), pt);
 
     srs                                = pt.get<std::string>("ModelMetadata.SRS");
-    std::string              srsOrigin = pt.get<std::string>("ModelMetadata.SRSOrigin");
-    std::vector<std::string> tokens;
-    boost::split(tokens, srsOrigin, boost::is_any_of(","));
+    if (boost::algorithm::contains(srs, "EPSG"))
+    {
+        std::string              srsOrigin = pt.get<std::string>("ModelMetadata.SRSOrigin");
+        std::vector<std::string> tokens;
+        boost::split(tokens, srsOrigin, boost::is_any_of(","));
 
-    if (tokens.size() == 3) {
-        x = std::stod(tokens[0]);
-        y = std::stod(tokens[1]);
-        z = std::stod(tokens[2]);
+        if (tokens.size() == 3) {
+            x = std::stod(tokens[0]);
+            y = std::stod(tokens[1]);
+            z = std::stod(tokens[2]);
+        }
+        else {
+            std::cout << "Invalid SRSOrigin format." << std::endl;
+        }
+        auto point = osgEarth::GeoPoint(
+            osgEarth::SpatialReference::create(srs), x, y, z, osgEarth::ALTMODE_ABSOLUTE);
+        std::cout << point.x() << ", " << point.y() << ", " << point.z() << std::endl;
+        return point;
     }
     else {
-        std::cout << "Invalid SRSOrigin format." << std::endl;
+        QRegExp rx("ENU:([0-9\\.]+), ([0-9\\.]+)");   // 正则表达式用于匹配数字
+        int     pos = rx.indexIn(srs.c_str());
+        if (pos > -1) {
+            QString lat = rx.cap(1);   // 第一个数字
+            QString lon = rx.cap(2);   // 第二个数字
+            std::cout << lat.toFloat() << " " << lon.toFloat() << std::endl;
+            auto point = osgEarth::GeoPoint(osgEarth::SpatialReference::get("wgs84"),
+                                            lon.toFloat(),
+                                            lat.toFloat(),
+                                            0,
+                                            osgEarth::ALTMODE_ABSOLUTE);
+            return point;
+            // do something with lat and lon
+        }
     }
-    auto point = osgEarth::GeoPoint(
-        osgEarth::SpatialReference::create(srs), x, y, z, osgEarth::ALTMODE_ABSOLUTE);
-    std::cout << point.x() << ", " << point.y() << ", " << point.z() << std::endl;
-    return point;
+    
 }
